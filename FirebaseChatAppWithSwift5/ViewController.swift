927583eb50beebae5d02601b0d7c5f5df4c7cd9a
nikolas <nikoagge@gmail.com>
2020-12-05 14:42:17 +0200

    Initial Commit

---
+++ FirebaseChatAppWithSwift5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseChatAppWithSwift5
+//
+//  Created by Nikolas Aggelidis on 5/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
